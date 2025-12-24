62810639b69a0ba18570cb841267b235adf9c9de
Nikhil <nikhil.4002.50.82@gmail.com>
2025-12-24 11:40:34 +0530

    setup next js

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-white h-screen'>
+      <h1 className='text-5xl text-red-600'>Setup Complete</h1>
+    </div>
+  )
+}
+
+export default Home
